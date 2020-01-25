c4006fed85fe83d72d84702fdb7fb13f7277ac5e
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-01-25 08:50:06 +0000

    first test of integration via cli

--- cli/index.js
+++ cli/index.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+const axios = require('axios');
+const { API_URL } = process.env;
 const argv = require('minimist')(process.argv.slice(2));
 const fs = require('fs');
 const generateId = () => Math.random().toString(36).substr(2, 5);
@@ -24,6 +27,13 @@ if (!fs.existsSync(f)) {
 }
 
 
-const content = fs.readFileSync(f).toString();
+const body = fs.readFileSync(f).toString();
 const slug = `${title.toLocaleLowerCase().replace(/\s/g, '-')}-${generateId()}`;
-console.log({ slug, title, content });
+console.log('sending post');
+axios
+    .post(`${API_URL}/posts`, { slug, title, body }, { headers: { authorization: key } })
+    .then(data => {
+        console.log('success');
+    }).catch(({ response }) => {
+        console.error('failed status code: ', response.status);
+    });
