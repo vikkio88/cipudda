d6e68693bd5d39e8287b23af4213999b08810589
Vincenzo Ciaccio <vincenzo.ciaccio@gmail.com>
2020-11-22 15:01:18 +0000

    removing sourcemaps from prod

--- frontend/rollup.config.js
+++ frontend/rollup.config.js
@@ -12,7 +12,7 @@ dotenv.config();
 export default {
 	input: 'src/main.js',
 	output: {
-		sourcemap: true,
+		sourcemap: !production,
 		format: 'iife',
 		name: 'app',
 		file: 'public/build/bundle.js'
